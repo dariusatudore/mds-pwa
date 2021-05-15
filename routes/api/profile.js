18c45eb2505c668c99c8fa70c297832d4920dade
Darius Atudore <dariusatud@gmail.com>
2021-05-15 16:27:52 +0300

    added profile POST request

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const auth = require("../../middleware/auth");
+const { check, validationResult } = require("express-validator");
 
 const Profile = require("../../models/Profile");
 const User = require("../../models/User");
@@ -25,4 +26,54 @@ router.get("/me", auth, async (req, res) => {
   }
 });
 
+// @route   POST api/profile
+// @desc    Create or update user profile
+// @access  Private
+router.post("/", [
+  auth,
+  [check("gender", "Gender is required").not().isEmpty()],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { images, location, gender, interests, bio, instagram } = req.body;
+
+    // build profile object
+    const profileFields = {};
+    profileFields.user = req.user.id;
+    if (location) profileFields.location = location;
+    if (gender) profileFields.gender = gender;
+    if (interests) profileFields.interests = interests;
+    if (bio) profileFields.bio = bio;
+    if (instagram) profileFields.instagram = instagram;
+    // if(images) {
+    //     profileFields.images = images.split(',').map(image => image.trim());
+    // }
+    if (images) profileFields.images = images;
+
+    try {
+      let profile = await Profile.findOne({ user: req.user.id });
+
+      if (profile) {
+        profile = await Profile.findOneAndUpdate(
+          { user: req.user.id },
+          { $set: profileFields },
+          { new: true }
+        );
+        return res.json(profile);
+      }
+
+      // create if not found
+      profile = new Profile(profileFields);
+
+      await Profile.save();
+      res.json(profile);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    }
+  },
+]);
 module.exports = router;
