7db1abedbb32cf3362702b9f5d99bf1f49170b14
Darius Atudore <dariusatud@gmail.com>
2021-05-15 13:22:54 +0300

    added user registration

--- config/db.js
+++ config/db.js
@@ -1,21 +1,24 @@
-const mongoose = require('mongoose');
-const config = require('config');
-const { connectableObservableDescriptor } = require('rxjs/internal/observable/ConnectableObservable');
-const db = config.get('mongoURI');
+const mongoose = require("mongoose");
+const config = require("config");
+const {
+  connectableObservableDescriptor,
+} = require("rxjs/internal/observable/ConnectableObservable");
+const db = config.get("mongoURI");
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(db, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
 
-        console.log('MongoDB connected...');
-    } catch(err) {
-        console.error(err.message);
-        // exit process with failure
-        process.exit(1);
-    }
-}
+    console.log("MongoDB connected...");
+  } catch (err) {
+    console.error(err.message);
+    // exit process with failure
+    process.exit(1);
+  }
+};
 
 module.exports = connectDB;
